4dceceb9b5359277427cf03355d2b873af0b17d0
Christian Jimenez <cjjimenez0913@gmail.com>
2019-10-29 01:17:01 -0500

    new push

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,45 @@
+var scrape = require("../scripts/scrape");
+
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes");
+
 module.exports = function(router) {
     router.get("/", function(req, res) {
         res.render("home");
     });
     router.get("/saved", function(req, res) {
         res.render("saved");
+    });
+
+    router.get("/api/fetch", function(req, res) {
+        headlinesController.fetch(function(err, docs) {
+            if (!docs || docs.insertedCounts === 0) {
+                res.json({
+                    message: "No new articles today. Check back tomorrow!"
+                });
+            }
+            else {
+                res.json({
+                    message: "Added " + docs.insertedCount + " new articles!"
+                });
+            }
+        });
+    });
+    router.get("/api/headlines", function(req, res) {
+        var query = {};
+        if (req.query.saved) {
+            query = req.query;
+        }
+        headlinesController.get(query, function(data){
+            res.json(data);
+        });
+    });
+
+    router.delete("/api/headlines/:id", function(req, res){
+        var query = {};
+        query._id = req.params.id;
+        headlinesController.delete(query, function(err, data){
+            res.json(data);
+        });
     });
 }
--- controllers/headlines.js
+++ controllers/headlines.js
@@ -28,5 +28,10 @@ module.exports = {
         .exec(function(err, doc) {
             cb(doc);
         });
+    },
+    update: function(query, cb) {
+        Headline.update({_id: query._id}, {
+            $set: query
+        }, {}, cb);
     }
-}
+};
